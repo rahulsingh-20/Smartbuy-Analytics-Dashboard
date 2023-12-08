576deda01d6144a32bd3493f6dd578b8313296d5
Rahul Singh <rahulapril20@gmail.com>
2023-12-08 23:51:50 +0530

    added middleware files for better error handling and user authentication

---
+++ backend/middleware/error.js
@@ -0,0 +1,35 @@
+const ErrorHandler = require("../utils/errorHandler");
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  // wrong mongodb id error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate key entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // duplicate JWT token error
+  if (err.name === "JsonWebTokenError") {
+    const message = "JSON Web Token is invalid, please try again";
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT expire error
+  if (err.name === "TokenExpiredError") {
+    const message = "JSON web token has expired, please try again";
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
